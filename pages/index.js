7c7e6e492e90077d4c03b5d879c77fb70ff5e3e0
Maxframe <wrnerm@gmail.com>
2022-05-16 23:27:29 +0200

    first commit + tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,65 @@
-import { useSession, signIn, signOut } from 'next-auth/react';
-import { useState } from 'react';
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
-  const [list, setList] = useState([]);
+  const [playlistList, setPlaylistList] = useState([]);
+  const [topArtistsList, setTopArtistsList] = useState([]);
 
   const getMyPlaylists = async () => {
-    const res = await fetch('/api/playlists');
+    const res = await fetch("/api/playlists");
     const { items } = await res.json();
-    setList(items);
+    setPlaylistList(items);
+  };
+  const getMyTopArtists = async () => {
+    const res = await fetch("/api/topartists");
+    const { items } = await res.json();
+    setTopArtistsList(items);
   };
 
   if (session) {
     return (
       <>
-        Signed in as {session?.token?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <div className="flex justify-between m-2">
+          Signed in as {session?.token?.email} <br />
+          <button
+            className="p-2 border bg-red-600 text-white rounded-lg"
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+        </div>
         <hr />
-        <button onClick={() => getMyPlaylists()}>Get all my playlists</button>
-        {list.map((item) => (
-          <div key={item.id}>
-            <h1>{item.name}</h1>
-            <img src={item.images[0]?.url} width='100' />
+        <div className="flex flex-row justify-around w-full m-2">
+          <div>
+            <button
+              className="p-2 border bg-green-600 text-white rounded-lg"
+              onClick={() => getMyPlaylists()}
+            >
+              Get all my playlists
+            </button>
+            {playlistList.map((item) => (
+              <div key={item.id}>
+                <h1>{item.name}</h1>
+                <img src={item.images[0]?.url} width="100" />
+              </div>
+            ))}
           </div>
-        ))}
+          <div>
+            <button
+              className="p-2 border bg-green-600 text-white rounded-lg"
+              onClick={() => getMyTopArtists()}
+            >
+              Get all my top artists
+            </button>
+            {topArtistsList.map((item) => (
+              <div key={item.id}>
+                <h1>{item.name}</h1>
+                <img src={item.images[0]?.url} width="100" />
+              </div>
+            ))}
+          </div>
+        </div>
       </>
     );
   }
